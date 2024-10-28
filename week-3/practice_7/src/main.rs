e5f0f8c7135113482bc00e05d54347ec1cffeef0
Byoc87 <roycascafelix@gmail.com>
2024-10-28 16:56:18 +0100

    (My third commit)

---
+++ week-3/practice_7/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+   let  float_with_seperator = 11_000.55_001;
+   println!("Float valus is {}", float_with_seperator);
+
+   let int_with_seperator = 50_000;
+   println!("Int value is {}", int_with_seperator);
+}
